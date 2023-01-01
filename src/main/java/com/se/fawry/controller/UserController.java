5c58cadaee2bb0390b0c6d9d4787f5f3aa2b004d
Youssef Nour <81862730+YoussefNour1@users.noreply.github.com>
2023-01-01 04:38:48 +0200

    update

--- src/main/java/com/se/fawry/controller/UserController.java
+++ src/main/java/com/se/fawry/controller/UserController.java
@@ -8,6 +8,7 @@ import com.se.fawry.enums.TransactionType;
 import com.se.fawry.model.repository.UserRepository;
 import com.se.fawry.service.Service;
 import com.se.fawry.service.UserService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -88,9 +89,10 @@ public class UserController {
         return ResponseEntity.ok(transaction);
     }
 
-    @PostMapping("/user/request/refund")
-    public ResponseEntity<RefundRequest> refund(@RequestBody RefundRequest refundRequest) {
-        RefundRequest createdRefundRequest = userService.requestRefund(refundRequest.getTransaction().getId(), refundRequest.getUser());
+    record RequestRefund(long userId, long transactionId){}
+    @PostMapping("/user/request-refund")
+    public ResponseEntity<RefundRequest> refund(@RequestBody RequestRefund refundRequest) {
+        RefundRequest createdRefundRequest = userService.requestRefund(refundRequest.transactionId, refundRequest.userId);
         if (createdRefundRequest == null) {
             return ResponseEntity.badRequest().build();
         }
@@ -99,11 +101,15 @@ public class UserController {
 
 
     @GetMapping("/user/discounts")
-    public List<Discount> getDiscounts(@RequestParam long serviceId, @RequestParam User user) {
-        return userService.getDiscounts(serviceId, user);
+    public List<Discount> getDiscounts() {
+        return userService.getOverAllDiscounts();
+    }
+    @GetMapping("/user/{userId}/{serviceId}/discounts")
+    public List<Discount> getDiscounts(@PathVariable long serviceId, @PathVariable long userId) {
+        return userService.getDiscounts(serviceId, userId);
     }
 
-    record AddService(String name, boolean cashOnDelivery, boolean creditCardPayment, String provider, ServiceType serviceType){}
+   /* record AddService(String name, boolean cashOnDelivery, boolean creditCardPayment, String provider, ServiceType serviceType){}
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/admin/add/service")
     public ResponseEntity<Service> addService(@RequestBody AddService service){
@@ -113,5 +119,6 @@ public class UserController {
     @GetMapping("/users")
     public List<User> all(){
         return userService.getAll();
-    }
+    }*/
+
 }
